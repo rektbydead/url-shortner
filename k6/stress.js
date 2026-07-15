4187247ed6d057189f012f74400882a300dcec19
Ruben Lousada <rlousada123456@gmail.com>
2026-07-15 22:15:52 +0100

    improve k6 test

--- k6/stress.js
+++ k6/stress.js
@@ -10,13 +10,44 @@ export const options = {
     ]
 }
 
-export default function () {
-    const res = http.get("http://backend:8000/shortner/d3e0fb69-aaf9-4162-a217-95bf7787b42a");
+const BASE_URL = "http://backend:8000/shortner"
+const uuidList = []
+
+function createShortUrl() {
+    const res = http.post(
+        `${BASE_URL}/`,
+        JSON.stringify({
+            original_url: "https://example.com/",
+            duration: 60,
+        }),
+        {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }
+    )
+
+    const body = res.json()
+    uuidList.push(body.uuid)
+}
+
+function search() {
+    const uuid = uuidList[Math.floor(Math.random() * uuidList.length)]
+    if (uuid === undefined) return
+
+    const res = http.get(`${BASE_URL}/${uuid}`)
 
     check(res, {
-        "status is 200": (r) => r.status === 200,
-        "status is 404": (r) => r.status === 404,
+        "GET 200": (r) => r.status === 200,
     })
+}
+
+export default function () {
+    if (Math.random() < 0.1) {
+        createShortUrl()
+    } else {
+         search()
+    }
 
     sleep(1)
 }
